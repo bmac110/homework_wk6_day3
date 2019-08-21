1eb0ae7b0cf1885e7ed695282ab86f39542dcea3
Billy Mackay <bossmack50@gmail.com>
2019-08-21 16:04:00 +0100

    journeys by transport passed

--- models/traveller.js
+++ models/traveller.js
@@ -5,20 +5,28 @@ const Traveller = function(journeys) {
 Traveller.prototype.getJourneyStartLocations = function() {
   const result = this.journeys.map((journey) => {
     return journey.startLocation;
-  })
+  });
   return result;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const result = this.journeys.filter(() => {
 
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
